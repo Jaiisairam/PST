64ad61bf5960f4291287e143e8445848eafe3ae5
jsr <vtu31736@veltech.edu.in>
2026-08-18 12:07:46 +0530

    Add files via upload

---
+++ compare the triplets.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+class Result {
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int aliceScore = 0;
+        int bobScore = 0;
+        for (int i = 0; i < 3; i++) {
+            if (a.get(i) > b.get(i)) {
+                aliceScore++;
+            } else if (a.get(i) < b.get(i)) {
+                bobScore++;
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        result.add(aliceScore);
+        result.add(bobScore);
+        return result;
+    }
+}
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+        List<Integer> result = Result.compareTriplets(a, b);
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i));
+            if (i != result.size() - 1) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        bufferedReader.close();
+    }
+}
